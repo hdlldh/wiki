c1d189766e38a08bf990e712b583cb967845666e
donglin <hdlldh@gmail.com>
2021-11-25 23:54:22 -0800

    add header to n-gram count files

--- handle_oov_and_generate_ngram.py
+++ handle_oov_and_generate_ngram.py
@@ -25,24 +25,28 @@ count = 0
 with gz.open(subject_file) as f:
     for line in f:
         count += 1
+        if count % 1000000 == 0: print("%s M sentences have been processed." % (count // 1000000))
         tokenized_sentence = tokenize_sentence(line.decode('UTF-8').strip())
         replaced_tokenized_sentence = replace_oov_words_by_unk(tokenized_sentence, vocab, unknown_token)
         replaced_tokenized_sentences.append(replaced_tokenized_sentence)
 
 unigram_count = count_n_grams(replaced_tokenized_sentences, 1, start_token, end_token)
 with gz.open(unigram_file, 'w') as f:
+    f.write(f"gram1,count\n".encode('UTF-8'))
     for k, v in unigram_count.items():
         w1 = k[0]
         f.write(f"{w1},{v}\n".encode('UTF-8'))
 
 bigram_count = count_n_grams(replaced_tokenized_sentences, 2, start_token, end_token)
 with gz.open(bigram_file, 'w') as f:
+    f.write(f"gram1,gram2,count\n".encode('UTF-8'))
     for k, v in bigram_count.items():
         w1, w2 = k
         f.write(f"{w1},{w2},{v}\n".encode('UTF-8'))
 
 trigram_count = count_n_grams(replaced_tokenized_sentences, 3, start_token, end_token)
 with gz.open(trigram_file, 'w') as f:
+    f.write(f"gram1,gram2,gram3,count\n".encode('UTF-8'))
     for k, v in trigram_count.items():
         w1, w2, w3 = k
         f.write(f"{w1},{w2},{w3},{v}\n".encode('UTF-8'))
